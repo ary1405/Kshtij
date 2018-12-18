350399432b7b415e189004811ea05e3db09f16f9
Shashi <shashi1iitk@gmail.com>
2018-12-18 15:03:20 +0530

    some changes

--- src/template/index.php
+++ src/template/index.php
@@ -15,9 +15,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     <meta name="google-site-verification" content="jHj7Q1niJT_o96E884V3BeKAgeVU18QGhRJK4Ah7PKg" />
     
-    <meta name="description" content="Kshitij, the annual techno-management fest of IIT Kharagpur is coming with 16th edition from 18th-20th January 2019.
-    While the competitions at Kshitij prove to be the ultimate test of grit, knowledge and skill, the guest lectures and workshops 
-      provide an enriching experience for the avid learners.">
+    <meta name="description" content="Kshitij, The Annual Techno-Management Fest of IIT Kharagpur is coming with its 16th edition from 18th-20th January 2019. Started in 2004, Kshitij was conceived with the aim to provide the technically inclined youth a national-level platform, where their talent and expertise would be recognized and rewarded.">
     <meta name="keywords" content="kshitij, ktj, iit, kharagpur, iit kharagpur, annual fest, technology, iit kgp, kgp">
 
     <base href="/" >
@@ -27,26 +25,20 @@
     <meta property="og:image" content="http://www.ktj.in/images/share/favicon.jpg"/>
     <meta property="og:url" content="http://www.ktj.in"/>
     <meta property="og:site_name" content="Kshitij 2019 | IIT Kharagpur"/>
-    <meta property="og:description" content="While the competitions at Kshitij prove to be the ultimate test of grit, knowledge and skill, the guest lectures and workshops 
-      provide an enriching experience for the avid learners.The fest has been graced by the presence of such luminaries who are looked
-      up to for their distinguished contribution in their respective fields. The upcoming 16th edition is going to be held from 18th-20th January 2019"/>
+    <meta property="og:description" content="Kshitij, The Annual Techno-Management Fest of IIT Kharagpur is coming with its 16th edition from 18th-20th January 2019. Started in 2004, Kshitij was conceived with the aim to provide the technically inclined youth a national-level platform, where their talent and expertise would be recognized and rewarded."/>
 
     <!-- Twitter meta -->
     <meta name="twitter:card" content="summary_large_image">
     <meta name="twitter:site" content="@Kshitij">
     <meta name="twitter:creator" content="@Kshitij">
     <meta name="twitter:title" content="Kshitij 2019 | 18-20th January | IIT Kharagpur">
-    <meta name="twitter:description" content="While the competitions at Kshitij prove to be the ultimate test of grit, knowledge and skill, the guest lectures and workshops 
-      provide an enriching experience for the avid learners.The fest has been graced by the presence of such luminaries who are looked
-      up to for their distinguished contribution in their respective fields. The upcoming 16th edition is going to be held from 18th-20th January 2019">
+    <meta name="twitter:description" content="Kshitij, The Annual Techno-Management Fest of IIT Kharagpur is coming with its 16th edition from 18th-20th January 2019. Started in 2004, Kshitij was conceived with the aim to provide the technically inclined youth a national-level platform, where their talent and expertise would be recognized and rewarded. While the competitions at Kshitij prove to be the ultimate test of grit, knowledge and skill, the guest lectures and workshops provide an enriching experience for the avid learners.The fest has been graced by the presence of such luminaries who are looked up to for their distinguished contribution in their respective fields.">
     <meta name="twitter:image" content="http://www.ktj.in/images/share/twitter.jpg">
     <meta name="twitter:image:src" content="http://www.ktj.in/images/share/twitter.jpg">
 
     <!-- GOOGLE + Share -->
     <meta itemprop="name" content="Kshitij 2019 | 18-20th January | IIT Kharagpur">
-    <meta itemprop="description" content="While the competitions at Kshitij prove to be the ultimate test of grit, knowledge and skill, the guest lectures and workshops 
-      provide an enriching experience for the avid learners.The fest has been graced by the presence of such luminaries who are looked
-      up to for their distinguished contribution in their respective fields. The upcoming 16th edition is going to be held from 18th-20th January 2019">
+    <meta itemprop="description" content="Kshitij, The Annual Techno-Management Fest of IIT Kharagpur is coming with its 16th edition from 18th-20th January 2019. Started in 2004, Kshitij was conceived with the aim to provide the technically inclined youth a national-level platform, where their talent and expertise would be recognized and rewarded. While the competitions at Kshitij prove to be the ultimate test of grit, knowledge and skill, the guest lectures and workshops provide an enriching experience for the avid learners.The fest has been graced by the presence of such luminaries who are looked up to for their distinguished contribution in their respective fields.">
     <meta itemprop="image" content="http://www.ktj.in/images/share/favicon.jpg">
 
     <!-- Favicons -->
