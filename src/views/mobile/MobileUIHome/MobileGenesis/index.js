785f37453b7fe12c17e87e1c6d94fb22db871764
blankRSD <shekhar.raj1610@gmail.com>
2018-12-21 15:18:32 +0530

    New mobile layout

--- src/views/mobile/MobileUIHome/MobileGenesis/index.js
+++ src/views/mobile/MobileUIHome/MobileGenesis/index.js
@@ -27,39 +27,16 @@ export default class MobileGenesis {
       titles: this._el.querySelectorAll('.js-genesis__title'),
       bodies: this._el.querySelectorAll('.js-genesis__body'),
       close: this._el.querySelector('.js-genesis__close'),
-      woodstock: this._el.querySelector('.woodstock_register'),
-      forex: this._el.querySelector('.forex_register'),
-      supply_matrix: this._el.querySelector('.supply_matrix_register'),
-      relic_hunter: this._el.querySelector('.relic_hunter_register'),
-      message: this._el.querySelectorAll('.register_message'),
+      tabcontainer: this._el.querySelector('.genesis__tab'),
+      tabcontentcontainer: this._el.querySelector('.genesis__tabcontent__container')
     };
-
-    var that = this;
-    this._ui.woodstock.addEventListener('click', function (e) {
-      return that.register(1);
-    });
-
-    var that = this;
-    this._ui.forex.addEventListener('click', function (e) {
-      return that.register(2);
-    });
-
-    var that = this;
-    this._ui.supply_matrix.addEventListener('click', function (e) {
-      return that.register(3);
-    });
-
-    var that = this;
-    this._ui.relic_hunter.addEventListener('click', function (e) {
-      return that.register(4);
-    });
-
-
 
     this._closeButton = new CloseButton({
       parent: this._ui.close,
       clickCallback: this._onCloseClick,
     });
+
+    this.geteventdata();
   }
 
   // State ---------------------------------------------------------------------
@@ -134,10 +111,165 @@ export default class MobileGenesis {
     States.router.navigateTo(pages.PROJECT, { id: projectList.projects[1].id });
   }
 
-  register(event_id) {
+  geteventdata() {
+    var that = this;
+    axios({
+      method: 'get',
+      url: 'https://api.ktj.in/events/1',
+      crossdomain: true,
+    })
+      .then(function (response) {
+        for (let eve of response.data) {
+          let btn = document.createElement('button');
+          btn.setAttribute('class', 'tablinks');
+          btn.setAttribute('id', 'tab' + eve.id);
+          let btntxt = document.createTextNode(eve.name);
+          btn.appendChild(btntxt);
+          that._ui.tabcontainer.appendChild(btn);
+
+          let tabcnt = document.createElement('div');
+          tabcnt.setAttribute('class', 'tabcontent');
+          tabcnt.setAttribute('id', 'tabcnt' + eve.id);
+
+          let bottomtabdiv = document.createElement('div');
+          bottomtabdiv.setAttribute('class', 'verttab');
+          bottomtabdiv.setAttribute('id', 'verttab' + eve.id);
+
+          let aboutbtn = document.createElement('button');
+          aboutbtn.setAttribute('class', 'verlinks');
+          aboutbtn.setAttribute('id', 'btnabout' + eve.id);
+          let abouttxt = document.createTextNode('About');
+          aboutbtn.appendChild(abouttxt);
+
+          let rulesbtn = document.createElement('button');
+          rulesbtn.setAttribute('class', 'verlinks');
+          rulesbtn.setAttribute('id', 'btnrules' + eve.id);
+          let rulestxt = document.createTextNode('Rules');
+          rulesbtn.appendChild(rulestxt);
+          let psbtn = document.createElement('button');
+          psbtn.setAttribute('class', 'verlinks');
+          psbtn.setAttribute('id', 'btnps' + eve.id);
+          let pstxt = document.createTextNode('PS');
+          psbtn.appendChild(pstxt);
+          let contactbtn = document.createElement('button');
+          contactbtn.setAttribute('class', 'verlinks');
+          contactbtn.setAttribute('id', 'btncontact' + eve.id);
+          let contacttxt = document.createTextNode('Contact');
+          contactbtn.appendChild(contacttxt);
+          let regisbtn = document.createElement('button');
+          regisbtn.setAttribute('class', 'verlinks');
+          regisbtn.setAttribute('id', 'btnregis' + eve.id);
+          let registxt = document.createTextNode('Register');
+          regisbtn.appendChild(registxt);
+
+          let eventwisediv = document.createElement('div');
+
+          let aboutdiv = document.createElement('div');
+          aboutdiv.setAttribute('class', 'verttabcontent');
+          aboutdiv.setAttribute('id', 'about' + eve.id);
+          aboutdiv.innerHTML = eve.about;
+
+          let rulesdiv = document.createElement('div');
+          rulesdiv.setAttribute('class', 'verttabcontent');
+          rulesdiv.setAttribute('id', 'rules' + eve.id);
+          rulesdiv.innerHTML = eve.rules;
+
+          let psdiv = document.createElement('div');
+          psdiv.setAttribute('class', 'verttabcontent');
+          psdiv.setAttribute('id', 'ps' + eve.id);
+          psdiv.innerHTML = eve.ps;
+
+          let contactdiv = document.createElement('div');
+          contactdiv.setAttribute('class', 'verttabcontent');
+          contactdiv.setAttribute('id', 'contact' + eve.id);
+          contactdiv.innerHTML = eve.contact;
+
+
+
+          let regisdiv = document.createElement('div');
+          regisdiv.setAttribute('class', 'verttabcontent');
+          regisdiv.setAttribute('id', 'regis' + eve.id);
+          let registext = document.createTextNode(eve.register);
+          regisdiv.appendChild(registext);
+
+
+          bottomtabdiv.appendChild(aboutbtn);
+          //
+          bottomtabdiv.appendChild(rulesbtn);
+          bottomtabdiv.appendChild(psbtn);
+          bottomtabdiv.appendChild(contactbtn);
+          bottomtabdiv.appendChild(regisbtn);
+
+          eventwisediv.appendChild(aboutdiv);
+          //
+          eventwisediv.appendChild(rulesdiv);
+          eventwisediv.appendChild(psdiv);
+          eventwisediv.appendChild(contactdiv);
+          eventwisediv.appendChild(regisdiv);
+
+          tabcnt.appendChild(bottomtabdiv);
+          tabcnt.appendChild(eventwisediv);
+
+          that._ui.tabcontentcontainer.appendChild(tabcnt);
+
+          btn.addEventListener('click', function (e) {
+            return that.clicktochangeevent(eve.id);
+          });
+          aboutbtn.addEventListener('click', function (e) {
+            return that.showeventdetail('about', eve.id);
+          });
+          // 
+          //   
+          // 
+          rulesbtn.addEventListener('click', function (e) {
+            return that.showeventdetail('rules', eve.id);
+          });
+          psbtn.addEventListener('click', function (e) {
+            return that.showeventdetail('ps', eve.id);
+          });
+          contactbtn.addEventListener('click', function (e) {
+            return that.showeventdetail('contact', eve.id);
+          });
+          regisbtn.addEventListener('click', function (e) {
+            return that.register(eve.id);
+          });
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+  }
+
+  clicktochangeevent(eventid) {
+    var that = this;
+    var len = that._ui.tabcontentcontainer.children.length;
+    for (let a = 0; a < len; a++) {
+      that._ui.tabcontainer.children[a].className = that._ui.tabcontainer.children[a].className.replace(" active", "");
+      that._ui.tabcontentcontainer.children[a].style.display = 'none';
+    }
+    that._el.querySelector('#tabcnt' + eventid).style.display = 'block';
+    that._el.querySelector('#tab' + eventid).className += " active";
+
+    that._el.querySelector('#about' + eventid).style.display = 'block';
+    that._el.querySelector('#btnabout' + eventid).className += " active";
+  }
+
+  showeventdetail(divtype, eventid) {
+    var that = this;
+    var detailtabs = that._el.querySelectorAll('.verlinks');
+    var detail = that._el.querySelectorAll('.verttabcontent')
+    for (let a = 0; a < detail.length; a++) {
+      detailtabs[a].className = detailtabs[a].className.replace(" active", "");
+      detail[a].style.display = 'none';
+    }
+    that._el.querySelector('#' + divtype + eventid).style.display = 'block';
+    that._el.querySelector('#btn' + divtype + eventid).className += " active";
+  }
+
+  register(eventid) {
     var datatosend = {
       'tokenval': localStorage.getItem('token') || '',
-      'eventid': event_id,
+      'eventid': eventid,
     };
 
     var that = this;
@@ -152,13 +284,13 @@ export default class MobileGenesis {
         'Content-Type': 'application/x-www-form-urlencoded',
       }
     })
-    .then(function (response) {
-      that._ui.message[event_id-1].innerHTML = response.data.message;
-      console.log(response.data);
-    })
-    .catch(function (error) {
-      console.log(error);
-    })
+      .then(function (response) {
+        that._el.querySelector('#regis' + eventid).innerHTML = 'Registration Status: ' + response.data.message + '<br/><a href="https://api.ktj.in/team" target="_blank">Click here</a> to create your team';
+        that.showeventdetail('regis', eventid);
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
   }
 
 
